64580b9aaf90a01beef3af562f790497e4f7b4c7
Gokleshchaudhary <123gopalchaudhary123@gmail.com>
2025-04-29 16:14:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function stringChop(str, size) {
   // your code here
+	for (let i = 0; i < str.length; i += chunkLength) {
+    // Use slice to get the chunk
+}
 }
 
 // Do not change the code below
