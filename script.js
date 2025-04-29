7b70c390c0dbf06dfc2273f357ef687f9ec93050
Gokleshchaudhary <123gopalchaudhary123@gmail.com>
2025-04-29 16:19:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 function stringChop(str, size) {
   // your code here
-	for (let i = 0; i < str.length; i += chunkLength) {
-    // Use slice to get the chunk
+	function chunkString(str, chunkLength) {
+    if (str === null) {
+        return [];
+    }
+
+    const chunks = [];
+
+    for (let i = 0; i < str.length; i += chunkLength) {
+        chunks.push(str.slice(i, i + chunkLength));
+    }
+
+    return chunks;
 }
+
+// Event listener for the button click
+document.getElementById('chunkButton').addEventListener('click', function() {
+    const str = document.getElementById('inputString').value;
+    const size = parseInt(document.getElementById('chunkSize').value, 10);
+
+    if (size > 0) {
+        const result = chunkString(str, size);
+        document.getElementById('result').innerText = `Chunks: ${JSON.stringify(result)}`;
+    } else {
+        document.getElementById('result').innerText = 'Please enter a valid chunk size.';
+    }
+});
 }
 
 // Do not change the code below
